0a2c1721fa15b451dae29ae27782d2bd12e5d6c9
alu0100512700 <alu0100512700@ull.edu.es>
2012-11-05 14:38:59 +0000

    pract7

--- lib/ratlib.rb
+++ lib/ratlib.rb
@@ -1,15 +1,18 @@
 class Ratnum
 
+include Comparable
+include Enumerable
+
 def initialize(a,b)
-@a, @b = a,b
+@numerador, @denominador = a,b
 end
 
 def num()
-@a
+@numerador
 end
 
-def denom()
-@b
+def den()
+@denominador
 end
 
 #def mostrar
@@ -17,15 +20,65 @@ end
 #end
 
 def numerador
-9
+@numerador
 end
 
 def denominador
-9
+@denominador
 end
 
-#def reduc
-#9,9
-#end
+def racional()
+return "#{@numerador}/#{@denominador}"
+end
+
+def flot()
+return ((@numerador.to_f)/(@denominador.to_f))
+end
+
+def == (ratnum1)
+if(ratnum1.num() == @numerador and (ratnum1.den() == @denominador))
+	return true
+else
+	return false
+end 
+end
+
+def abs
+if @numerador < 0
+	return Ratnum.new(-@numerador,@denominador)
+else
+	return self
+end
+end
+
+def recip
+return Ratnum.new(@denominador,@numerador)
+end
+
+def -@
+Ratnum.new(-@numerador,@denominador)
+end
+
+def +(ratnum1)
+if (ratnum1.instance_of?(Ratnum) == true)
+	aux1=(@numerador * ratnum1.den()) + (@denominador * ratnum1.num())
+	aux2=(@denominador * ratnum1.den())
+	i=1
+	aux=1
+	while((i < aux1+1) and (i < aux2+1))do
+		if((aux1%i==0) and (aux2%i==0))
+			aux=i
+		end
+		i=i+1
+	end
+	aux1=aux1/aux
+	aux2=aux2/aux
+	return Racional.new(aux1,aux2)
+end
+end
+
+
+
 
 end
+
